81fbe9df2ba898ae82a6e2c70c925e1d1248d36a
p22yxi <p22yxi@fairfieldprep.org>
2021-11-29 11:00:19 -0500

    lab_1

---
+++ lab_1-1.py
@@ -0,0 +1,9 @@
+# Author Yongdong Xi
+def greeting():
+    '''Prints "Hello World"'''
+    print("Hello World")
+
+
+greeting()
+
+# help(greeting)
---
+++ lab_1-2.py
@@ -0,0 +1,8 @@
+# Author Yongdong Xi
+def find_sum(a, b):
+    c = a + b
+    return c
+
+
+my_sum = find_sum(111, 222)
+print(my_sum)
