6dfcaa443451601a6aafbbac3796cb96d85ddac0
p22yxi <p22yxi@fairfieldprep.org>
2021-12-08 19:10:53 -0800

    lab2-2, finished

--- lab_2-2.py
+++ lab_2-2.py
@@ -1,8 +1,9 @@
 # Yongdong Xi
 
-def party_invites(namelst):
-    for x in namelst:
+def party_invites(namelst1):
+    for x in namelst1:
         print("Hi {0}, You're invited to my party on Friday!". format(x))
 
 
-print(party_invites(["Chen", 'Yu', 'Jie']))
+namelst = ['Chen', 'Yu', "Jie"]
+print(party_invites(namelst))
